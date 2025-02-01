3aa65de1bb4b0e5a4e33c066891ba5c6cfc3b867
tingwei628 <sbvq6p199xe@gmail.com>
2025-02-01 22:37:47 +0800

    feat: add named pipe socket

---
+++ npcs/NamedPipeSocket_Client/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.IO.Pipes;
+using System.Net.Sockets;
+using System.Text;
+
+using var client = new NamedPipeClientStream(".", "mypipe", PipeDirection.InOut);
+await client.ConnectAsync();
+Console.WriteLine("server connected");
+
+string message = "Hello from Client!";
+await client.WriteAsync(Encoding.UTF8.GetBytes(message));
+
+
+
+byte[] buffer = new byte[256];
+int bytesRead = await client.ReadAsync(buffer);
+message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+Console.WriteLine($"message from server: {message}");
+;
---
+++ npcs/NamedPipeSocket_Server/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.IO.Pipes;
+using System.Text;
+
+//Windows Named Pipes
+using var server = new NamedPipeServerStream("mypipe", PipeDirection.InOut, 1);
+Console.WriteLine("wait for client...");
+await server.WaitForConnectionAsync();
+Console.WriteLine("client connected !");
+
+byte[] buffer = new byte[256];
+int bytesRead = await server.ReadAsync(buffer);
+var message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+Console.WriteLine($"message from client: {message}");
+
+string response = "Hello from Server!";
+await server.WriteAsync(Encoding.UTF8.GetBytes(response));
+//await server.FlushAsync();
+;
